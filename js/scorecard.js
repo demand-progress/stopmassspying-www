37f7fbdfacb1dfd51e4ab89dc341a5ab161b259f
Chris Antaki <ChrisAntaki@gmail.com>
2016-02-01 14:29:17 -0800

    Using WOFF fonts

--- js/scorecard.js
+++ js/scorecard.js
@@ -1,3 +1,16 @@
+// Check for outdated browsers
+var isIE = navigator.userAgent.match(/MSIE (\d+)\./);
+if (isIE) {
+    var version = +isIE[1];
+    if (version < 9) {
+        alert('Unfortunately your browser, Internet Explorer ' + version + ', is not supported.\nPlease visit the site with a modern browser like Firefox or Chrome.\nThanks!');
+    }
+}
+
+if (navigator.userAgent.match(/Android 2\.3/)) {
+    alert('Unfortunately your browser, Android 2.3, is not supported.\nPlease visit the site with a modern browser like Firefox or Chrome.\nThanks!');
+}
+
 $(function() {
     // Fix potential click delay on mobile devices
     FastClick.attach(document.body);
