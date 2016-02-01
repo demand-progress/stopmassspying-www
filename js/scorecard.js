902d077bb9cb6572e6157b69a47637b54d59e774
Chris Antaki <ChrisAntaki@gmail.com>
2016-01-31 19:48:23 -0800

    Added a few simpler dropdowns

---
+++ js/scorecard.js
@@ -0,0 +1,19 @@
+$(function() {
+    $('.candidates').on('click', '.candidate', function(e) {
+        var isOpened = !$(this).hasClass('opened');
+        var message = isOpened ? 'Read less' : 'Read more';
+
+        $(this).toggleClass('opened');
+        $(this).find('.read-more').text(message);
+    });
+
+    $('.candidates').on('click', '.dropdown', function(e) {
+        e.stopPropagation();
+    });
+
+    $('.candidates').on('click', '.subject', function(e) {
+        e.stopPropagation();
+
+        console.log($(this));
+    });
+});
